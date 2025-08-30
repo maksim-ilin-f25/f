9788573351affac7f2463110c21f1ab83d206371
Asqar Arslanov <asqarslanov@gmail.com>
2025-08-31 00:28:26 +0300

    this

--- app/src/main/kotlin/university/innopolis/f/grammar/FIdentifier.kt
+++ app/src/main/kotlin/university/innopolis/f/grammar/FIdentifier.kt
@@ -4,9 +4,9 @@ import FLetter.Companion.toFLetterOrNull
 @JvmInline
 value class FIdentifier(private val name: String) {
     init {
-        require(name.first().toFLetterOrNull() != null)
+        require(this.name.first().toFLetterOrNull() != null)
         require(
-                name.drop(1).all {
+                this.name.drop(1).all {
                     it.toFLetterOrNull() != null || it.toFDecimalDigitOrNull() != null
                 },
         )
--- app/src/main/kotlin/university/innopolis/f/utils/BigUInteger.kt
+++ app/src/main/kotlin/university/innopolis/f/utils/BigUInteger.kt
@@ -3,7 +3,7 @@ import java.math.BigInteger
 @JvmInline
 value class BigUInteger(private val inner: BigInteger) {
     init {
-        require(inner > 0.toBigInteger())
+        require(this.inner > 0.toBigInteger())
     }
 
     companion object {
