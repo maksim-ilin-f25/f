6fd1f0c609b057375d8b3a9efe2d54f0099850f5
Asqar Arslanov <asqarslanov@gmail.com>
2025-09-11 14:35:08 +0300

    fix ident bug

--- app/src/main/kotlin/university/innopolis/f/grammar/FIdentifier.kt
+++ app/src/main/kotlin/university/innopolis/f/grammar/FIdentifier.kt
@@ -1,23 +1,6 @@
 package university.innopolis.f.grammar
 
-import university.innopolis.f.grammar.FDecimalDigit.Companion.toFDecimalDigitOrNull
-import university.innopolis.f.grammar.FLetter.Companion.toFLetterOrNull
-
 @JvmInline
 value class FIdentifier(private val name: String) {
-    init {
-        require(this.name.first().toFLetterOrNull() != null)
-        require(
-            this.name.drop(1).all {
-                it.toFLetterOrNull() != null || it.toFDecimalDigitOrNull() != null
-            }
-        )
-    }
-
     override fun toString() = """Identifier("${this.name}")"""
-
-    companion object {
-        fun String.toFIdentifierOrNull(): FIdentifier? =
-            runCatching { FIdentifier(this) }.getOrNull()
-    }
 }
