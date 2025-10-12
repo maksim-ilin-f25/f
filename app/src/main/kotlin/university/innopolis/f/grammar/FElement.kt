1a02a35b6ddebf774bb62d692fe81dbed9267d46
can4red <CAN4red@yandex.ru>
2025-10-12 18:34:32 +0300

    .

--- app/src/main/kotlin/university/innopolis/f/grammar/FElement.kt
+++ app/src/main/kotlin/university/innopolis/f/grammar/FElement.kt
@@ -2,32 +2,22 @@ package university.innopolis.f.grammar
 
 sealed class FElement {
     data class Atom(val value: FAtom) : FElement() {
-        override fun toString(): String {
-            return super.toString()
-        }
+        override fun toString() = value.toString()
     }
 
     data class Literal(val value: FLiteral) : FElement() {
-        override fun toString(): String {
-            return super.toString()
-        }
+        override fun toString() = value.toString()
     }
 
     data class List(val value: FList) : FElement() {
-        override fun toString(): String {
-            return super.toString()
-        }
+        override fun toString() = value.toString()
     }
 
     data class Quote(val value: FElement) : FElement() {
-        override fun toString(): String {
-            return super.toString()
-        }
+        override fun toString() = value.toString()
     }
 
     data class Keyword(val value: FKeyword) : FElement() {
-        override fun toString(): String {
-            return super.toString()
-        }
+        override fun toString() = value.toString()
     }
 }
