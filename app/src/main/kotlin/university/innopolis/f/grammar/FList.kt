b217d5189a221e8c1250e4e22de96447262f35a7
can4red <CAN4red@yandex.ru>
2025-10-12 18:10:26 +0300

    .

--- app/src/main/kotlin/university/innopolis/f/grammar/FList.kt
+++ app/src/main/kotlin/university/innopolis/f/grammar/FList.kt
@@ -5,10 +5,17 @@ import university.innopolis.f.lexer.FToken
 import university.innopolis.f.parser.ParseException
 
 @JvmInline
-value class FList(private val elements: MutableList<FElement>) {
+value class FList(private val _elements: MutableList<FElement>) {
+    val elements: List<FElement>
+        get() = _elements
+
     companion object {
         /** On success, returns the index after the matching closing parenthesis. */
-        fun parse(allTokens: List<FToken>, firstElemIndex: Int): Result<Pair<FList, Int>> {
+        fun parse(
+            allTokens: List<FToken>,
+            firstElemIndex: Int,
+            isFirstRun: Boolean,
+        ): Result<Pair<FList, Int>> {
             val self = FList(emptyList<FElement>().toMutableList())
 
             var res = Pair(firstElemIndex, true)
@@ -17,8 +24,9 @@ value class FList(private val elements: MutableList<FElement>) {
                     parseElement(
                             allTokens = allTokens,
                             currentElemIndex = res.first,
-                            buffer = self.elements,
+                            buffer = self._elements,
                             openParCoordinate = allTokens[firstElemIndex - 1].coordinate,
+                            isFirstRun = isFirstRun,
                         )
                         .getOrElse {
                             return Result.failure(it)
@@ -33,6 +41,7 @@ value class FList(private val elements: MutableList<FElement>) {
             currentElemIndex: Int,
             buffer: MutableList<FElement>,
             openParCoordinate: Coordinate,
+            isFirstRun: Boolean,
         ): Result<Pair<Int, Boolean>> {
             val currentToken = allTokens.getOrNull(currentElemIndex)
             if (currentToken == null) {
@@ -41,13 +50,23 @@ value class FList(private val elements: MutableList<FElement>) {
             when (currentToken) {
                 is FToken.OpeningParenthesis -> { // recursion
                     val (listAst, nextIndex) =
-                        parse(allTokens, currentElemIndex + 1).getOrElse {
-                            return Result.failure(it)
-                        }
+                        parse(
+                                allTokens = allTokens,
+                                firstElemIndex = currentElemIndex + 1,
+                                isFirstRun = false,
+                            )
+                            .getOrElse {
+                                return Result.failure(it)
+                            }
                     buffer.add(FElement.List(listAst))
                     return Result.success(Pair(nextIndex, true))
                 }
-                is FToken.ClosingParenthesis -> { // exit success
+                is FToken.ClosingParenthesis -> {
+                    if (isFirstRun) {
+                        return Result.failure(
+                            ParseException.UnmatchedClosingParen(currentToken.coordinate)
+                        )
+                    }
                     return Result.success(Pair(currentElemIndex + 1, false))
                 }
                 is FToken.Atom -> {
@@ -56,17 +75,23 @@ value class FList(private val elements: MutableList<FElement>) {
                 is FToken.Literal -> {
                     buffer.add(FElement.Literal(currentToken.value))
                 }
+                is FToken.Keyword -> {
+                    buffer.add(FElement.Keyword(currentToken.value))
+                }
                 is FToken.Quote -> {
                     val res =
-                        parseElement(allTokens, currentElemIndex + 1, buffer, openParCoordinate)
+                        parseElement(
+                                allTokens = allTokens,
+                                currentElemIndex = currentElemIndex + 1,
+                                buffer = buffer,
+                                openParCoordinate = openParCoordinate,
+                                isFirstRun = false,
+                            )
                             .getOrElse {
                                 return Result.failure(it)
                             }
                     buffer[buffer.lastIndex] = FElement.Quote(buffer.last())
                     return Result.success(res)
-                }
-                is FToken.Keyword -> {
-                    buffer.add(FElement.Keyword(currentToken.value))
                 }
             }
 
