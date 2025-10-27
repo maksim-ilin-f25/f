c626065b2c33021c4ee720c608a3f36dbfe07687
can4red <CAN4red@yandex.ru>
2025-10-27 20:21:48 +0300

    .

--- app/src/main/kotlin/university/innopolis/f/runtime/Runtime.kt
+++ app/src/main/kotlin/university/innopolis/f/runtime/Runtime.kt
@@ -2,12 +2,29 @@ package university.innopolis.f.runtime
 
 import university.innopolis.f.grammar.FElement
 
-fun run(ast: List<FElement>): Iterator<Result<String>> {
+fun run(ast: List<FElement>): Sequence<Result<String>> {
     return Runtime(ast).run()
 }
 
 class Runtime(val ast: List<FElement>) {
-    fun run(): Iterator<Result<String>> {
-        TODO()
+    fun run(): Sequence<Result<String>> = sequence {
+        outer@ for (element in ast) {
+            for (result in runElement(element)) {
+                yield(result)
+                if (result.isFailure) {
+                    break@outer
+                }
+            }
+        }
+    }
+
+    fun runElement(element: FElement): Iterator<Result<String>> {
+        when (element) {
+            is FElement.Atom -> TODO()
+            is FElement.Keyword -> TODO()
+            is FElement.List -> TODO()
+            is FElement.Literal -> TODO()
+            is FElement.Quote -> TODO()
+        }
     }
 }
