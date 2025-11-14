a32e6ba75b762f2f994681b2d7b563e49a878a76
can4red <CAN4red@yandex.ru>
2025-11-14 20:28:56 +0300

    .

--- app/src/main/kotlin/university/innopolis/f/runtime/Runtime.kt
+++ app/src/main/kotlin/university/innopolis/f/runtime/Runtime.kt
@@ -1,21 +1,31 @@
-// package university.innopolis.f.runtime
-//
-// import university.innopolis.f.grammar.FAtom
-// import university.innopolis.f.grammar.FElement
-//
+package university.innopolis.f.runtime
+
+import university.innopolis.f.grammar.FElement
+import university.innopolis.f.grammar.FElementQuoted
+
 // fun runF(ast: List<FElement>): Sequence<Result<String>> {
 //    val rootContext = FContext(parent = null)
 //
-//    rootContext.set(FAtom("plus"), FValue.Function(FFunction.Builtin { args, context -> plus(args,
-// context) }))
-//    rootContext.set(FAtom("minus"), FValue.Function(FFunction.Builtin { args, context ->
-// minus(args, context) }))
-//    rootContext.set(FAtom("times"), FValue.Function(FFunction.Builtin { args, context ->
-// times(args, context) }))
-//    rootContext.set(FAtom("divide"), FValue.Function(FFunction.Builtin { args, context ->
-// divide(args, context) }))
-//    rootContext.set(FAtom("head"), FValue.Function(FFunction.Builtin { args, context -> head(args,
-// context) }))
+//    rootContext.set(
+//        FAtom("plus"),
+//        FValue.Function(FFunction.Builtin { args, context -> plus(args, context) }),
+//    )
+//    rootContext.set(
+//        FAtom("minus"),
+//        FValue.Function(FFunction.Builtin { args, context -> minus(args, context) }),
+//    )
+//    rootContext.set(
+//        FAtom("times"),
+//        FValue.Function(FFunction.Builtin { args, context -> times(args, context) }),
+//    )
+//    rootContext.set(
+//        FAtom("divide"),
+//        FValue.Function(FFunction.Builtin { args, context -> divide(args, context) }),
+//    )
+//    rootContext.set(
+//        FAtom("head"),
+//        FValue.Function(FFunction.Builtin { args, context -> head(args, context) }),
+//    )
 //
 //    val context = FContext(parent = rootContext)
 //    return sequence {
@@ -30,85 +40,84 @@
 //        }
 //    }
 // }
-//
-// fun evaluateListTo(list: MutableList<FValue>, ast: List<FElement>, context: FContext):
-// Sequence<Result<FValue>> =
-//    sequence {
-//        for (outputSequence in ast) {
-//            var element: FValue? = null
-//            TODO("runElement no longer exists")
-//            for (result in runElement(outputSequence, context)) {
-//                yield(result)
-//                if (result.isFailure) {
-//                    return@sequence
-//                }
-//                element = result.getOrThrow()
-//            }
-//            list.add(element!!)
-//        }
-//
-//    }
-//
-// class Wrapper<T>(var value: T)
-//
-// fun evaluateElementTo(
-//    target: Wrapper<FValue>,
-//    element: FElement,
-//    context: FContext
-// ): Sequence<Result<FValue>> =
-//    sequence {
-//        when (element) {
-//            is FElement.Atom -> {
-//                val value = context.valueOf(element.value)
-//                if (value == null) {
-//                    yield(Result.failure(FRuntimeException.UnboundAtom()))
-//                    return@sequence
-//                }
-//                target.value = value
-//            }
-//
-//            is FElement.List -> {
-//                val funCall = element.value.toFunCallOrNull()
-//                if (funCall == null) {
-//                    yield(Result.failure(FRuntimeException.MalformedFunCall()))
-//                    return@sequence
-//                }
-//                val function = context.valueOf(funCall.name)
-//                if (function == null) {
-//                    yield(Result.failure(FRuntimeException.UnboundAtom()))
-//                    return@sequence
-//                }
-//                if (function !is FValue.Function) {
-//                    yield(Result.failure(FRuntimeException.NotAFunction()))
-//                    return@sequence
-//                }
-//
-//                val args = mutableListOf<FValue>()
-//                for (result in evaluateListTo(args, funCall.args, context)) {
-//                    yield(result)
-//                    if (result.isFailure) {
-//                        return@sequence
-//                    }
-//                }
-//
-//                for (result in function.value.call(args, context)) {
-//                    yield(result)
-//                    if (result.isFailure) {
-//                        return@sequence
-//                    }
-//                }
-//
-//                TODO("write the list to `target`")
-//            }
-//
-//            is FElement.Literal -> {
-//                target.value = FValue.fromLiteral(element.value)
-//            }
-//
-//            is FElement.Quote -> {
-//                target.value = FValue.Quote(element.value)
-//            }
-//
-//            is FElement.Keyword -> yield(Result.failure(FRuntimeException.StandaloneKeyword()))
-//        }
-//    }
+
+fun evaluateListTo(
+    list: MutableList<FValue>,
+    ast: List<FElement>,
+    context: FContext,
+): Sequence<Result<FValue>> = sequence {
+    for (outputSequence in ast) {
+        val element: Wrapper<FValue?> = Wrapper(null)
+        for (result in evaluateElementTo(element, outputSequence, context)) {
+            yield(result)
+            if (result.isFailure) {
+                return@sequence
+            }
+        }
+        list.add(element.value!!)
+    }
+}
+
+class Wrapper<T>(var value: T)
+
+fun evaluateElementTo(
+    target: Wrapper<FValue?>,
+    element: FElement,
+    context: FContext,
+): Sequence<Result<FValue>> = sequence {
+    target.value =
+        when (element) {
+            is FElement.Quote -> {
+                when (element.value) {
+                    is FElementQuoted.Atom -> {
+                        val value = context.valueOf(element.value.value)
+                        if (value == null) {
+                            yield(Result.failure(FRuntimeException.UnboundAtom()))
+                            return@sequence
+                        }
+                        value
+                    }
+                    is FElementQuoted.List -> FValue.Quote(FValueQuoted.ValueList(TODO()))
+                    is FElementQuoted.Literal -> FValue.fromLiteral(element.value.value)
+                    is FElementQuoted.Quote -> FValue.Quote(TODO())
+                    is FElementQuoted.Keyword -> {
+                        yield(Result.failure(FRuntimeException.StandaloneKeyword()))
+                        return@sequence
+                    }
+                }
+            }
+            is FElement.List -> {
+                val funCall = element.value.toFunCallOrNull()
+                if (funCall == null) {
+                    yield(Result.failure(FRuntimeException.MalformedFunCall()))
+                    return@sequence
+                }
+                val function = context.valueOf(funCall.name)
+                if (function == null) {
+                    yield(Result.failure(FRuntimeException.UnboundAtom()))
+                    return@sequence
+                }
+                if (function !is FValue.Function) {
+                    yield(Result.failure(FRuntimeException.NotAFunction()))
+                    return@sequence
+                }
+
+                val args = mutableListOf<FValue>()
+                for (result in evaluateListTo(args, funCall.args, context)) {
+                    yield(result)
+                    if (result.isFailure) {
+                        return@sequence
+                    }
+                }
+
+                for (result in function.value.call(args, context)) {
+                    yield(result)
+                    if (result.isFailure) {
+                        return@sequence
+                    }
+                }
+
+                TODO("put here the return value for `target`")
+            }
+        }
+}
