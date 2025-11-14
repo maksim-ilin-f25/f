b85277e30f143dd5d215bc0fbfc6e6cf074fabaa
can4red <CAN4red@yandex.ru>
2025-11-14 20:10:19 +0300

    .

--- app/src/main/kotlin/university/innopolis/f/runtime/BuiltinFunction.kt
+++ app/src/main/kotlin/university/innopolis/f/runtime/BuiltinFunction.kt
@@ -1,123 +1,275 @@
-// package university.innopolis.f.runtime
-//
-// import university.innopolis.f.grammar.FElement
-// import university.innopolis.f.grammar.FInteger
-// import university.innopolis.f.grammar.FReal
-//
-// fun plus(args: List<FValue>, _context: FContext): Sequence<Result<FValue>> = sequence {
-//    if (args.size != 2) {
-//        yield(Result.failure(FRuntimeException.InvalidNumOfArgs()))
-//        return@sequence
-//    }
-//    val (lhs, rhs) = args
-//    yield(
-//        when {
-//            lhs is FValue.Integer && rhs is FValue.Integer ->
-//                Result.success(FValue.Integer(FInteger(lhs.value.inner + rhs.value.inner)))
-//
-//            lhs is FValue.Integer && rhs is FValue.Real ->
-//                Result.success(FValue.Real(FReal(lhs.value.inner.toBigDecimal() +
-// rhs.value.inner)))
-//
-//            lhs is FValue.Real && rhs is FValue.Integer ->
-//                Result.success(FValue.Real(FReal(lhs.value.inner +
-// rhs.value.inner.toBigDecimal())))
-//
-//            lhs is FValue.Real && rhs is FValue.Real ->
-//                Result.success(FValue.Real(FReal(lhs.value.inner + rhs.value.inner)))
-//
-//            else -> Result.failure(FRuntimeException.TypeError())
-//        }
-//    )
-// }
-//
-// fun minus(args: List<FValue>, _context: FContext): Sequence<Result<FValue>> = sequence {
-//    if (args.size != 2) {
-//        yield(Result.failure(FRuntimeException.InvalidNumOfArgs()))
-//        return@sequence
-//    }
-//    val (lhs, rhs) = args
-//    yield(
-//        when {
-//            lhs is FValue.Integer && rhs is FValue.Integer ->
-//                Result.success(FValue.Integer(FInteger(lhs.value.inner - rhs.value.inner)))
-//
-//            lhs is FValue.Integer && rhs is FValue.Real ->
-//                Result.success(FValue.Real(FReal(lhs.value.inner.toBigDecimal() -
-// rhs.value.inner)))
-//
-//            lhs is FValue.Real && rhs is FValue.Integer ->
-//                Result.success(FValue.Real(FReal(lhs.value.inner -
-// rhs.value.inner.toBigDecimal())))
-//
-//            lhs is FValue.Real && rhs is FValue.Real ->
-//                Result.success(FValue.Real(FReal(lhs.value.inner - rhs.value.inner)))
-//
-//            else -> Result.failure(FRuntimeException.TypeError())
-//        }
-//    )
-// }
-//
-// fun times(args: List<FValue>, _context: FContext): Sequence<Result<FValue>> = sequence {
-//    if (args.size != 2) {
-//        yield(Result.failure(FRuntimeException.InvalidNumOfArgs()))
-//        return@sequence
-//    }
-//    val (lhs, rhs) = args
-//    yield(
-//        when {
-//            lhs is FValue.Integer && rhs is FValue.Integer ->
-//                Result.success(FValue.Integer(FInteger(lhs.value.inner * rhs.value.inner)))
-//
-//            lhs is FValue.Integer && rhs is FValue.Real ->
-//                Result.success(FValue.Real(FReal(lhs.value.inner.toBigDecimal() *
-// rhs.value.inner)))
-//
-//            lhs is FValue.Real && rhs is FValue.Integer ->
-//                Result.success(FValue.Real(FReal(lhs.value.inner *
-// rhs.value.inner.toBigDecimal())))
-//
-//            lhs is FValue.Real && rhs is FValue.Real ->
-//                Result.success(FValue.Real(FReal(lhs.value.inner * rhs.value.inner)))
-//
-//            else -> Result.failure(FRuntimeException.TypeError())
-//        }
-//    )
-// }
-//
-// fun divide(args: List<FValue>, _context: FContext): Sequence<Result<FValue>> = sequence {
-//    if (args.size != 2) {
-//        yield(Result.failure(FRuntimeException.InvalidNumOfArgs()))
-//        return@sequence
-//    }
-//    val (lhs, rhs) = args
-//
-//    try {
-//        yield(
-//            when {
-//                lhs is FValue.Integer && rhs is FValue.Integer ->
-//
-// Result.success(FValue.Integer(FInteger(lhs.value.inner.divide(rhs.value.inner))))
-//
-//                lhs is FValue.Integer && rhs is FValue.Real ->
-//
-// Result.success(FValue.Real(FReal(lhs.value.inner.toBigDecimal().divide(rhs.value.inner))))
-//
-//                lhs is FValue.Real && rhs is FValue.Integer ->
-//
-// Result.success(FValue.Real(FReal(lhs.value.inner.divide(rhs.value.inner.toBigDecimal()))))
-//
-//                lhs is FValue.Real && rhs is FValue.Real ->
-//                    Result.success(FValue.Real(FReal(lhs.value.inner.divide(rhs.value.inner))))
-//
-//                else -> Result.failure(FRuntimeException.TypeError())
-//            }
-//        )
-//    } catch (_: Exception) {
-//        yield(Result.failure(FRuntimeException.DivisionByZero()))
-//    }
-// }
-//
+package university.innopolis.f.runtime
+
+import university.innopolis.f.grammar.FInteger
+import university.innopolis.f.grammar.FReal
+
+fun plus(args: List<FValue>, _context: FContext): Sequence<Result<FValue>> = sequence {
+    if (args.size != 2) {
+        yield(Result.failure(FRuntimeException.InvalidNumOfArgs()))
+        return@sequence
+    }
+    val (lhs, rhs) = args
+    yield(
+        when {
+            lhs is FValue.Quote &&
+                lhs.value is FValueQuoted.Integer &&
+                rhs is FValue.Quote &&
+                rhs.value is FValueQuoted.Integer -> {
+                Result.success(
+                    FValue.Quote(
+                        FValueQuoted.Integer(
+                            FInteger(lhs.value.value.inner + rhs.value.value.inner)
+                        )
+                    )
+                )
+            }
+
+            lhs is FValue.Quote &&
+                lhs.value is FValueQuoted.Integer &&
+                rhs is FValue.Quote &&
+                rhs.value is FValueQuoted.Real -> {
+                Result.success(
+                    FValue.Quote(
+                        FValueQuoted.Real(
+                            FReal(lhs.value.value.inner.toBigDecimal() + rhs.value.value.inner)
+                        )
+                    )
+                )
+            }
+
+            lhs is FValue.Quote &&
+                lhs.value is FValueQuoted.Real &&
+                rhs is FValue.Quote &&
+                rhs.value is FValueQuoted.Integer -> {
+                Result.success(
+                    FValue.Quote(
+                        FValueQuoted.Real(
+                            FReal(lhs.value.value.inner + rhs.value.value.inner.toBigDecimal())
+                        )
+                    )
+                )
+            }
+
+            lhs is FValue.Quote &&
+                lhs.value is FValueQuoted.Real &&
+                rhs is FValue.Quote &&
+                rhs.value is FValueQuoted.Real -> {
+                Result.success(
+                    FValue.Quote(
+                        FValueQuoted.Real(FReal(lhs.value.value.inner + rhs.value.value.inner))
+                    )
+                )
+            }
+
+            else -> Result.failure(FRuntimeException.TypeError())
+        }
+    )
+}
+
+fun minus(args: List<FValue>, _context: FContext): Sequence<Result<FValue>> = sequence {
+    if (args.size != 2) {
+        yield(Result.failure(FRuntimeException.InvalidNumOfArgs()))
+        return@sequence
+    }
+    val (lhs, rhs) = args
+    yield(
+        when {
+            lhs is FValue.Quote &&
+                lhs.value is FValueQuoted.Integer &&
+                rhs is FValue.Quote &&
+                rhs.value is FValueQuoted.Integer -> {
+                Result.success(
+                    FValue.Quote(
+                        FValueQuoted.Integer(
+                            FInteger(lhs.value.value.inner - rhs.value.value.inner)
+                        )
+                    )
+                )
+            }
+
+            lhs is FValue.Quote &&
+                lhs.value is FValueQuoted.Integer &&
+                rhs is FValue.Quote &&
+                rhs.value is FValueQuoted.Real -> {
+                Result.success(
+                    FValue.Quote(
+                        FValueQuoted.Real(
+                            FReal(lhs.value.value.inner.toBigDecimal() - rhs.value.value.inner)
+                        )
+                    )
+                )
+            }
+
+            lhs is FValue.Quote &&
+                lhs.value is FValueQuoted.Real &&
+                rhs is FValue.Quote &&
+                rhs.value is FValueQuoted.Integer -> {
+                Result.success(
+                    FValue.Quote(
+                        FValueQuoted.Real(
+                            FReal(lhs.value.value.inner - rhs.value.value.inner.toBigDecimal())
+                        )
+                    )
+                )
+            }
+
+            lhs is FValue.Quote &&
+                lhs.value is FValueQuoted.Real &&
+                rhs is FValue.Quote &&
+                rhs.value is FValueQuoted.Real -> {
+                Result.success(
+                    FValue.Quote(
+                        FValueQuoted.Real(FReal(lhs.value.value.inner - rhs.value.value.inner))
+                    )
+                )
+            }
+
+            else -> Result.failure(FRuntimeException.TypeError())
+        }
+    )
+}
+
+fun times(args: List<FValue>, _context: FContext): Sequence<Result<FValue>> = sequence {
+    if (args.size != 2) {
+        yield(Result.failure(FRuntimeException.InvalidNumOfArgs()))
+        return@sequence
+    }
+    val (lhs, rhs) = args
+    yield(
+        when {
+            lhs is FValue.Quote &&
+                lhs.value is FValueQuoted.Integer &&
+                rhs is FValue.Quote &&
+                rhs.value is FValueQuoted.Integer -> {
+                Result.success(
+                    FValue.Quote(
+                        FValueQuoted.Integer(
+                            FInteger(lhs.value.value.inner * rhs.value.value.inner)
+                        )
+                    )
+                )
+            }
+
+            lhs is FValue.Quote &&
+                lhs.value is FValueQuoted.Integer &&
+                rhs is FValue.Quote &&
+                rhs.value is FValueQuoted.Real -> {
+                Result.success(
+                    FValue.Quote(
+                        FValueQuoted.Real(
+                            FReal(lhs.value.value.inner.toBigDecimal() * rhs.value.value.inner)
+                        )
+                    )
+                )
+            }
+
+            lhs is FValue.Quote &&
+                lhs.value is FValueQuoted.Real &&
+                rhs is FValue.Quote &&
+                rhs.value is FValueQuoted.Integer -> {
+                Result.success(
+                    FValue.Quote(
+                        FValueQuoted.Real(
+                            FReal(lhs.value.value.inner * rhs.value.value.inner.toBigDecimal())
+                        )
+                    )
+                )
+            }
+
+            lhs is FValue.Quote &&
+                lhs.value is FValueQuoted.Real &&
+                rhs is FValue.Quote &&
+                rhs.value is FValueQuoted.Real -> {
+                Result.success(
+                    FValue.Quote(
+                        FValueQuoted.Real(FReal(lhs.value.value.inner * rhs.value.value.inner))
+                    )
+                )
+            }
+
+            else -> Result.failure(FRuntimeException.TypeError())
+        }
+    )
+}
+
+fun divide(args: List<FValue>, _context: FContext): Sequence<Result<FValue>> = sequence {
+    if (args.size != 2) {
+        yield(Result.failure(FRuntimeException.InvalidNumOfArgs()))
+        return@sequence
+    }
+    val (lhs, rhs) = args
+
+    try {
+        yield(
+            when {
+                lhs is FValue.Quote &&
+                    lhs.value is FValueQuoted.Integer &&
+                    rhs is FValue.Quote &&
+                    rhs.value is FValueQuoted.Integer -> {
+                    Result.success(
+                        FValue.Quote(
+                            FValueQuoted.Integer(
+                                FInteger(lhs.value.value.inner.divide(rhs.value.value.inner))
+                            )
+                        )
+                    )
+                }
+
+                lhs is FValue.Quote &&
+                    lhs.value is FValueQuoted.Integer &&
+                    rhs is FValue.Quote &&
+                    rhs.value is FValueQuoted.Real -> {
+                    Result.success(
+                        FValue.Quote(
+                            FValueQuoted.Real(
+                                FReal(
+                                    lhs.value.value.inner
+                                        .toBigDecimal()
+                                        .divide(rhs.value.value.inner)
+                                )
+                            )
+                        )
+                    )
+                }
+
+                lhs is FValue.Quote &&
+                    lhs.value is FValueQuoted.Real &&
+                    rhs is FValue.Quote &&
+                    rhs.value is FValueQuoted.Integer -> {
+                    Result.success(
+                        FValue.Quote(
+                            FValueQuoted.Real(
+                                FReal(
+                                    lhs.value.value.inner.divide(
+                                        rhs.value.value.inner.toBigDecimal()
+                                    )
+                                )
+                            )
+                        )
+                    )
+                }
+
+                lhs is FValue.Quote &&
+                    lhs.value is FValueQuoted.Real &&
+                    rhs is FValue.Quote &&
+                    rhs.value is FValueQuoted.Real -> {
+                    Result.success(
+                        FValue.Quote(
+                            FValueQuoted.Real(
+                                FReal(lhs.value.value.inner.divide(rhs.value.value.inner))
+                            )
+                        )
+                    )
+                }
+
+                else -> Result.failure(FRuntimeException.TypeError())
+            }
+        )
+    } catch (_: Exception) {
+        yield(Result.failure(FRuntimeException.DivisionByZero()))
+    }
+}
+
 // fun head(args: List<FValue>, context: FContext): Sequence<Result<FValue>> = sequence {
 //    if (args.size != 1) {
 //        yield(Result.failure(FRuntimeException.InvalidNumOfArgs()))
